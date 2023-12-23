5be853d3dd70f4e6f0d207f113c1a5cab6aab161
muhmdrdho <muhmdrdho@gmail.com>
2023-12-24 03:37:44 +0700

    upday

--- app.py
+++ app.py
@@ -6,19 +6,5 @@ import database as db
 
 users = db.fetch_all_users()
 
-usernames = [user["key"] for user in users]
-names = [user["name"] for user in users]
-hashed_passwords = [user["password"] for user in users]
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
 
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-
-if authentication_status:
-    st.write("you did this")
--- database.py
+++ database.py
@@ -39,21 +39,3 @@ def delete_user(username):
     """Always returns None, even if the key does not exist"""
     return db.delete(username)
 
-users = fetch_all_users()
-
-usernames = [user["key"] for user in users]
-names = [user["name"] for user in users]
-hashed_passwords = [user["password"] for user in users]
-
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
-
-name, authentication_status, username = authenticator.login("Login", "main")
-
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-
-if authentication_status:
-    st.write("you did this")
