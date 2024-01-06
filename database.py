ef34535ed57f96240ef944e63ee7e0f6fdd0d54c
muhmdrdho <muhmdrdho@gmail.com>
2024-01-06 22:54:12 +0700

    feat: convert json to shp then upload

--- database.py
+++ database.py
@@ -1,23 +1,104 @@
-import streamlit_antd_components as sac
 import streamlit as st
+import folium
+from folium import plugins
+from streamlit_folium import folium_static
+from src.database.db import deta
+from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map
+import geopandas as gpd
+from geojson_transformer import GeoJsonTransformer
+from gpx_converter import Converter
+import gpxpy 
+import gpxo
+import os 
+from io import StringIO
+import os
+from zipfile import ZipFile 
 
-def nav():
-    with st.sidebar:
-        option_menu = sac.menu([
-                        sac.MenuItem('Dashboard', 
-                                     icon='cast'),
-                        sac.MenuItem('Explore', 
-                                     icon='compass', 
-                                     children=[
-                                        sac.MenuItem('Database', 
-                                                     icon='apple'),
-                        ]),
-                        sac.MenuItem('About', 
-                                     icon='question-circle',
-                                     children=[
-                                         sac.MenuItem('How it works?',
-                                                      icon='gear')
-                                         
-                                     ]),    
-                    ], format_func='title', size='small', indent=10, open_all=True, return_index=True)
-nav()
+def upload_deta(collection: str, filename: str, byteCode: bytes) -> str:
+    gpxDrive = deta.Drive(collection)
+    gpxData = gpxDrive.put(filename, byteCode)
+
+    return gpxData
+
+def get_deta_list():
+    gpxFile_db = 'gpxFile_db'
+    gpxDrive = deta.Drive(gpxFile_db)
+    gpxData_list = gpxDrive.list(100)
+
+    return gpxData_list['names']
+
+def get_deta_file(filename: str):
+    gpxFile_db = 'gpxFile_db'
+    gpxDrive = deta.Drive(gpxFile_db)
+    return gpxDrive.get(filename)
+
+def delete_file(filename: str):
+    if os.path.exists(filename):
+        os.remove(filename)
+    else:
+        print(f"The file {filename} does not exist")
+
+def convert_file_to_shp(source: str, output: str) -> str:
+    gdf = gpd.read_file(source)
+    gdf.to_file(output)
+    return output
+
+
+def upload_maps():
+    uploaded = st.file_uploader('Choose your file', accept_multiple_files=True)
+    for uploads in uploaded:
+        
+        stringio = StringIO(uploads.getvalue().decode('utf-8'))
+        string_data = stringio.read()
+
+        gpxData = upload_deta("gpxFile_db", uploads.name, uploads.getvalue())
+
+    
+    data_list = get_deta_list()
+    choose_data = st.selectbox("Select data to see view", data_list)
+    comp_data = get_deta_file(choose_data)
+    binary_data = comp_data.read()
+
+    fileName = choose_data
+    with open(fileName, "wb") as binary_file:
+        # Write bytes to file
+        binary_file.write(binary_data)
+    
+    extractedFileName = f"extract_{fileName}"
+    jsonFile = GeoJsonTransformer(fileName).save_geojson(filepath=extractedFileName)
+    
+    filezz, ext = fileName.split('.')
+
+    with open(fileName, "rb") as file:
+        # Write bytes to file
+        output = file.read()
+        extracted = upload_deta("JsonFile_db", jsonFile.name, output)
+
+    delete_file(fileName)
+
+    shp = convert_file_to_shp(jsonFile.name, filezz + ".shp")
+    delete_file(jsonFile.name)
+
+     # writing files to a zipfile 
+    with ZipFile(f'{filezz}.zip','w') as zip: 
+        # writing each file one by one 
+        files = [f'{filezz}.shp', f'{filezz}.cpg', f'{filezz}.dbf', f'{filezz}.prj', f'{filezz}.shx']
+        for file in files: 
+            zip.write(file)
+
+    for file in files:
+        delete_file(file)
+
+    
+    # upload zip
+    with open(f'{filezz}.zip', "rb") as file:
+        # Write bytes to file
+        output = file.read()
+        upload_deta("shpFile_db", f'{filezz}.zip', output)
+
+    delete_file(f'{filezz}.zip')
+    
+
+    
+
+upload_maps()
