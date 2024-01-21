409865b24989747b36782df24162c942da46a29d
muhmdrdho <muhmdrdho@gmail.com>
2024-01-21 13:46:38 +0700

    update

--- src/components/pages/layout.py
+++ src/components/pages/layout.py
@@ -1,5 +1,8 @@
-from src.components.settings.pages_set import *
 import src.components.pages.authentication.auth as nauth
+from src.components.settings.pages_set import set_page
+def page_settings():
+    set = set_page()
+    return set
 
 def logPage():
     log = nauth.logPart()
