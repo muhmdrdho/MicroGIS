c808471be426787dd9eeddab599e17f3e4cb8ee5
muhmdrdho <muhmdrdho@gmail.com>
2024-01-21 14:46:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import src.major as major
+import streamlit as st 
+
+hide_st_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    header {visibility: hidden;}
+                    </style>
+                    """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 microgis = major.main_layout()
